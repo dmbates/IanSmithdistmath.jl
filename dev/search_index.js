aadf80aeeea9023891154578c4436346a0949e5e
Documenter.jl <documenter@juliadocs.github.io>
2022-02-24 21:50:07 +0000

    build based on c274cd5

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = IanSmithdistmath","category":"page"},{"location":"#IanSmithdistmath","page":"Home","title":"IanSmithdistmath","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for IanSmithdistmath.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [IanSmithdistmath]","category":"page"}]
+}
